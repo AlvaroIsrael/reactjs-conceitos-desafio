2b172a8f9b7df4935cd0fb0c12805d47ace33172
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-04-07 19:52:25 -0300

    feature: Implemented feature witch allows users to add a new repository to the backend.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 import './styles.css';
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(res => {
+      setRepositories(res.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    /*This is just a Mock, a form does not exist yet.*/
+    const dados = {
+      id: '123',
+      url: 'https://github.com/AlvaroIsrael',
+      title: 'Desafio ReactJS',
+      techs: ['React', 'Node.js', 'React Native'],
+    };
+    const response = await api.post('/repositories', dados);
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +31,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => {
+          return <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>;
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
